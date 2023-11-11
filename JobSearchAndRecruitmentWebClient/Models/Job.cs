6c74949280d869bcdc5e5b8a23cb107a62f10d49
Trần Trung Việt <trungviet2k1@gmail.com>
2023-11-12 06:50:37 +0700

    jobAPI

---
+++ JobSearchAndRecruitmentWebClient/Models/Job.cs
@@ -0,0 +1,29 @@
+﻿using BusinessObject.Commons;
+
+namespace JobSearchAndRecruitmentWebClient.Models
+{
+    public class Job
+    {
+        public int JobId { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Industry { get; set; }
+
+        public string? Location { get; set; }
+
+        public DateTime? ApplicationDeadline { get; set; }
+
+        public string? RequiredSkills { get; set; }
+
+        public string? RequiredEducation { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public TimeType? TimeType { get; set; }
+
+        public int EmployerId { get; set; }
+    }
+}
